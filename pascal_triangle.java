9a41e8284696ff7cd46fc5f70a2c5b94a66b966d
s1u2k3 <bsukhendu48@gmail.com>
2025-08-14 16:59:42 +0000

    days3_challenges

---
+++ pascal_triangle.java
@@ -0,0 +1,16 @@
+public class pascal_triangle {
+    public static void main(String[] args) {
+        int rows = 6;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < rows - i; j++) {
+                System.out.print(" ");
+            }
+            int num = 1;
+            for (int k = 0; k <= i; k++) {
+                System.out.print(num + " ");
+                num = num * (i - k) / (k + 1);
+            }
+            System.out.println();
+        }
+    }
+}
