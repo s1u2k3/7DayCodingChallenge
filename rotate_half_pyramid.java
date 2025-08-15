6b1fe3cb5828b3e3c78ea61f360fa70181e3f649
s1u2k3 <bsukhendu48@gmail.com>
2025-08-15 17:26:47 +0000

    days4_challenges

---
+++ rotate_half_pyramid.java
@@ -0,0 +1,16 @@
+public class rotate_half_pyramid {
+    public static void main(String[]args){
+        int n = 5;
+
+        for(int i = n; i > 0; i--){
+            for(int j = 0; j < n - i; j++){
+                System.out.print("");
+            }
+
+            for(int k = 0; k < i; k++){
+                System.out.print("$ ");
+            }
+            System.out.println();
+        }
+    }
+}
