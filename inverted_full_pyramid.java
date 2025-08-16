70217422c8c50d25518508d3dd2c8a563c6fbe53
s1u2k3 <bsukhendu48@gmail.com>
2025-08-16 16:10:47 +0000

    days_5_challenges

--- inverted_full_pyramid.java
+++ inverted_full_pyramid.java
@@ -1,14 +1,13 @@
-public class rotate_half_pyramid {
-    public static void main(String[]args){
+public class inverted_full_pyramid {
+    public static void main(String[] args) {
         int n = 5;
-
         for(int i = n; i > 0; i--){
             for(int j = 0; j < n - i; j++){
                 System.out.print(" ");
             }
 
-            for(int k = 0; k < i; k++){
-                System.out.print("$ ");
+            for(int k = 0; k < 2*i-1; k++){
+                System.out.print("@");
             }
             System.out.println();
         }
